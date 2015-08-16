d6c7153ce4e38689df8da559b612a8f0daee6bf0
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2015-08-16 19:44:02 +0200

    hw: Remove the power on/off sequences
    For now we do all the power state handling in the isp code.
    
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- bcwc_hw.c
+++ bcwc_hw.c
@@ -668,46 +668,6 @@ static int bcwc_hw_irq_disable(struct bcwc_private *dev_priv)
 	return 0;
 }
 
-static int bcwc_hw_sensor_enable(struct bcwc_private *dev_priv)
-{
-	return 0;
-}
-
-static int bcwc_hw_sensor_disable(struct bcwc_private *dev_priv)
-{
-	return 0;
-}
-
-static int bcwc_hw_s2chip_enable(struct bcwc_private *dev_priv)
-{
-	return 0;
-}
-
-static int bcwc_hw_s2chip_disable(struct bcwc_private *dev_priv)
-{
-	return 0;
-}
-
-/* What I've figured out about the power on sequence so far */
-static int bcwc_hw_power_on(struct bcwc_private *dev_priv)
-{
-	bcwc_hw_s2chip_enable(dev_priv);
-	bcwc_hw_sensor_enable(dev_priv);
-	bcwc_hw_irq_enable(dev_priv);
-
-	return 0;
-}
-
-/* What I've figured out about the power off sequence so far */
-static int bcwc_hw_power_off(struct bcwc_private *dev_priv)
-{
-	bcwc_hw_irq_disable(dev_priv);
-	bcwc_hw_sensor_disable(dev_priv);
-	bcwc_hw_s2chip_disable(dev_priv);
-
-	return 0;
-}
-
 int bcwc_hw_init(struct bcwc_private *dev_priv)
 {
 	u32 val;
